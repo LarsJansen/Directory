1b0772af13391e30e9e96a72f4ad5d5ee0660e0e
Lars Jansen <larsborg@gmail.com>
2026-03-28 19:28:05 +1300

    Rename app to Internet History Directory
    Update application name in config/app.php from 'Directory MVP' to 'Internet History Directory'. This updates the displayed/project name used by the app configuration.

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'name' => 'Directory MVP',
+    'name' => 'Internet History Directory',
     'base_url' => '',
     'session_name' => 'dmoz_mvp_session',
     'debug' => true,
